1edcb5759147b9374f1459bd35e43de7ee8937a7
Axel Niklasson <axel.niklasson@live.com>
2019-07-05 10:42:34 +0200

    Concurrent cleanup

--- lib/commands/cleanup.go
+++ lib/commands/cleanup.go
@@ -1,15 +1,31 @@
 package commands
 
-import "log"
+import (
+	"log"
+	"sync"
+)
+
+func performCleanup(sliceName string, hostname string, onDone func()) {
+	log.Printf("Cleaning up node %s", hostname)
+	err := ExecCmdOnNode(sliceName, hostname, "rm -rf ~/app", false)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = ExecCmdOnNode(sliceName, hostname, "kill -9 -1", false)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer onDone()
+}
 
 // Cleanup performs a node cleanup on the supplied hostname(s)
 func Cleanup(sliceName string, hostnames []string) error {
+	var wg sync.WaitGroup
 	for _, hostname := range hostnames {
-		log.Printf("Cleaning up node %s", hostname)
-		ExecCmdOnNode(sliceName, hostname, "rm -rf ~/app", false)
-		ExecCmdOnNode(sliceName, hostname, "kill -9 -1", false)
+		wg.Add(1)
+		go performCleanup(sliceName, hostname, wg.Done)
 	}
-
+	wg.Wait()
 	log.Print("Cleanup completed")
 	return nil
 }
