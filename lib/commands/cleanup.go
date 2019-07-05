2745c7d1eef969613ef5cff6b9489294d95be6ee
Axel Niklasson <axel.niklasson@live.com>
2019-07-05 10:47:03 +0200

    Refactor cleanup code with anonymous func

--- lib/commands/cleanup.go
+++ lib/commands/cleanup.go
@@ -5,27 +5,29 @@ import (
 	"sync"
 )
 
-func performCleanup(sliceName string, hostname string, onDone func()) {
-	log.Printf("Cleaning up node %s", hostname)
-	err := ExecCmdOnNode(sliceName, hostname, "rm -rf ~/app", false)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = ExecCmdOnNode(sliceName, hostname, "kill -9 -1", false)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer onDone()
-}
-
 // Cleanup performs a node cleanup on the supplied hostname(s)
 func Cleanup(sliceName string, hostnames []string) error {
 	var wg sync.WaitGroup
+
 	for _, hostname := range hostnames {
 		wg.Add(1)
-		go performCleanup(sliceName, hostname, wg.Done)
+		go func(hostname string) {
+			defer wg.Done()
+
+			log.Printf("Cleaning up node %s", hostname)
+			err := ExecCmdOnNode(sliceName, hostname, "rm -rf ~/app", false)
+			if err != nil {
+				log.Fatal(err)
+			}
+			err = ExecCmdOnNode(sliceName, hostname, "kill -9 -1", false)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}(hostname)
 	}
+
 	wg.Wait()
 	log.Print("Cleanup completed")
+
 	return nil
 }
